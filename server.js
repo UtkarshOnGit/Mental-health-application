75a2aea6e573b797790b6b734f92dd28276dd01b
UtkarshOnGit <pandey.utkarsh1112@gmail.com>
2026-03-16 16:53:52 +0530

    Configure socket.io with CORS settings
    Enable CORS for socket.io connections.

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+//const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '/')));
